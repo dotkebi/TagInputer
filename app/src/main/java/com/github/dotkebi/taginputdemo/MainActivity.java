dce35c091ca5f6db6b0ae91c8c3250df861fbc96
dotkebi <dotkebi@gmail.com>
2016-05-07 22:59:07 +0900

    separate listener interface

--- app/src/main/java/com/github/dotkebi/taginputdemo/MainActivity.java
+++ app/src/main/java/com/github/dotkebi/taginputdemo/MainActivity.java
@@ -5,11 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import com.github.dotkebi.taginput.OnInputTagListener;
 import com.github.dotkebi.taginput.TagInputer;
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity implements TagInputer.OnInputTagListener {
+public class MainActivity extends AppCompatActivity implements OnInputTagListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
